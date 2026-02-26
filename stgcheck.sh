c702993b13ed067afa8dd6548585a37d4972d472
Dan-herete <danheret@gmail.com>
2026-02-26 15:26:45 -0500

    First version release to check storage usage

---
+++ stgcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "===== FULL SYSTEM STORAGE REPORT ====="
+echo ""
+
+echo "Top 30 Largest Directories & Files (Entire System):"
+echo ""
+
+sudo du -ahx / 2>/dev/null | sort -rh | head -n 30
+
+echo ""
+echo "======================================="
+echo "Done."
